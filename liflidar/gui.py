c9fe184e5060550d7bc24f8753003eb5786be6ad
sia <yakubov.sha@gmail.com>
2024-05-22 14:12:35 +0200

    Add multiple plots

--- liflidar/gui.py
+++ liflidar/gui.py
@@ -17,54 +17,53 @@ import sys
 
 import matplotlib
 
-matplotlib.use('Qt5Agg')
+matplotlib.use("Qt5Agg")
 
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg  # , NavigationToolbar2QT
-from matplotlib.figure import Figure
 from PyQt5 import QtCore, QtWidgets
 
 
 class DataHandler:
-
     def __init__(self, instrument):
+        n_plots = 2
         n_data = 50
         self.instrument = instrument()
-        self._xdata = list(range(n_data))
-        self._ydata = [0 for _ in range(n_data)]
+        self._xdata = [list(range(n_data)) for _ in range(n_plots)]
+        self._ydata = [[0 for _ in range(n_data)] for _ in range(n_plots)]
 
     def update_ydata(self):
         # Drop off the first y element, append a new one.
-        self._ydata = self._ydata[1:] + [random.randint(0, 10)]
-    
+        for i in range(len(self._ydata)):
+            self._ydata[i] = self._ydata[i][1:] + [random.randint(0, 10)]
+
     def empty_axes_data(self):
         return self._xdata, self._ydata
-    
+
     @property
     def ydata(self):
         self.update_ydata()
         return self._ydata
 
-class MplCanvas(FigureCanvasQTAgg):
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=20, height=7, dpi=300):
+        fig, self.axes = plt.subplots(nrows=1, ncols=2, figsize=(width, height))  # , dpi=dpi)
         super().__init__(fig)
 
 
 class MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self, instrument, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.data_handler = DataHandler(instrument)
+        self.data_handler = DataHandler(instrument)  # get data handler instead of instrument
 
         self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
         self.setCentralWidget(self.canvas)
 
         # We need to store a reference to the plotted line
         # somewhere, so we can apply the new data to it.
-        self._plot_ref = None
+        self._plot_refs = [None, None]
         self.initial_plot()
         self.show()
 
@@ -73,18 +72,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.setInterval(100)
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
-    
+
     def initial_plot(self):
         # First time we have no plot reference, so do a normal plot.
         # .plot returns a list of line <reference>s, as we're
         # only getting one we can take the first element.
         xdata, ydata = self.data_handler.empty_axes_data()
-        plot_refs = self.canvas.axes.plot(xdata, ydata, 'r')
-        self._plot_ref = plot_refs[0]
+        for i, (ax, x, y) in enumerate(zip(self.canvas.axes, xdata, ydata, strict=True)):
+            plot_ref = ax.plot(x, y, "r")
+            self._plot_refs[i] = plot_ref[0]
 
     def update_plot(self):
         # We have a reference, we can use it to update the data for that line.
-        self._plot_ref.set_ydata(self.data_handler.ydata)
+        for plot_ref, ydata in zip(self._plot_refs, self.data_handler.ydata, strict=True):
+            plot_ref.set_ydata(ydata)
         # Trigger the canvas to update and redraw.
         self.canvas.draw()
 
